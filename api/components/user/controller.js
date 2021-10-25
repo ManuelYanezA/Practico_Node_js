4ea12f0dd90b7d5536ab0eb898059a111ab15878
ManuelYanezA <manuel.yanez1501@alumnos.ubiobio.cl>
2021-10-25 12:32:04 -0300

    Agregado sistema de caché

--- api/components/user/controller.js
+++ api/components/user/controller.js
@@ -3,13 +3,25 @@ const auth = require('../auth');
 
 const TABLA = 'user';
 
-module.exports = function(injectedStore){
+module.exports = function(injectedStore, injectedCache){
+    let cache = injectedCache;
     let store = injectedStore;
     if(!store){
         store = require('../../../store/dummy');
     }
-    function list(){
-        return store.list(TABLA);
+    if(!cache){
+        cache = require('../../../store/dummy');
+    }
+    async function list(){
+        let users = await cache.list(TABLA);
+        if(!users){
+            console.log('No estaba en caché, buscando en la BD');
+            users = await store.list(TABLA);
+            cache.upsert(TABLA, users);
+        } else {
+            console.log('Nos traemos datos de caché');
+        }
+        return users;
     }
     function get(id){
         return store.list(TABLA, id);
--- config.js
+++ config.js
@@ -10,10 +10,10 @@ module.exports = {
         secret: process.env.JWT_SECRET || '',
     },
     mysql: {
-        host: process.env.MYSQL_HOST || '',
-        user: process.env.MYSQL_USER || '',
-        password: process.env.MYSQL_PASS || '',
-        database: process.env.MYSQL_DB || '',
+        host: process.env.MYSQL_HOST || 'remotemysql.com',
+        user: process.env.MYSQL_USER || 'VjtEvKIH1q',
+        password: process.env.MYSQL_PASS || 'MOhocDtnDq',
+        database: process.env.MYSQL_DB || 'VjtEvKIH1q',
     },
     mysqlService: {
         host: process.env.MYSQL_SRV_HOST || '',
@@ -23,4 +23,9 @@ module.exports = {
         host: process.env.CACHE_SRV_HOST || '',
         port: process.env.CACHE_SRV_PORT || 3003,
     },
+    redis: {
+        host: process.env.REDIS_HOST || 'redis-14604.c291.ap-southeast-2-1.ec2.cloud.redislabs.com',
+        port: process.env.REDIS_PORT || 14604,
+        password: process.env.REDIS_PASS || 'Mj2DGMOlu66mQyedSbjfcNCKk6NWZFJJ'
+    }
 }
